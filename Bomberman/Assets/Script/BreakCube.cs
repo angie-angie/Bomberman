0192db75c141d179a31ddee48f42fd2fa5d0e02c
angie2018 <angie55angie33@gmail.com>
2019-01-21 12:20:41 +0900

    ステージ作成

---
+++ Bomberman/Assets/Script/BreakCube.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BreakCube : MonoBehaviour {
+
+    //こっちは中身のマップ用のプレハブ
+    public GameObject BreakCubePrefab;
+    public int count = 10;
+
+    void Start()
+    {
+        //これがマップの元になるデータ
+        string map_matrix = "1111111111111111111:" +
+                            "1111111111111111111:" +
+                            "1111111111111111111:" +
+                            "1110111011101110111:" +
+                            "1111111111111111111:" +
+                            "1111111111111111111:" +
+                            "1111111111111111111:" +
+                            "1110111011101110111:" +
+                            "1111111111111111111:" +
+                            "1111111111111111111:" +
+                            "1111111111111111111:" +
+                            "1110111011101110111:" +
+                            "1111111111111111111:" +
+                            "1111111111111111111:" +
+                            "1111111111111111111:" +
+                            "1110111011101110111:" +
+                            "1111111111111111111:" +
+                            "1111111111111111111:" +
+                            "1111111111111111111:";
+
+        // 引数にこれを入れてマップ生成する
+        CreateMap(map_matrix);
+    }
+
+    //マップを作るメソッド
+    void CreateMap(string map_matrix)
+    {
+        //「:」をデリミタとして、map_matrix_arrに配列として分割していれます
+        string[] map_matrix_arr = map_matrix.Split(':');
+
+        //map_matrix_arrの配列の数を最大値としてループ
+        for (int x = 0; x < map_matrix_arr.Length; x++)
+        {
+            //xを元に配列の要素を取り出す
+            string x_map = map_matrix_arr[x];
+            //１配列に格納されている文字の数でx軸をループ
+            for (int z = 0; z < x_map.Length; z++)
+            {
+                //配列から取り出した１要素には111011011011011011こんな値が入っているのでこれを１文字づつ取り出す
+                int obj = int.Parse(x_map.Substring(z, 1));
+
+                //もしも０だったら壁ということで壁のプレハブをインスタンス化してループして出したx座標z座標を指定して設置
+                //if (obj == 0)
+                //{
+                //    Instantiate(CubePrefab, new Vector3(x + 1, 0, z + 1), Quaternion.identity);
+                //}
+
+                //もしも2だったら壁ということで壁のプレハブをインスタンス化してループして出したx座標z座標を指定して設置
+                if (obj == 2)
+                {
+                    Instantiate(BreakCubePrefab, new Vector3(x + 1, 0, z + 1), Quaternion.identity);
+                }
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Bomberman/Assets/Script/MapGenerator.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapGenerator : MonoBehaviour{
+
+    public GameObject defaultWallPrefab;
+    private int default_x_max = 20;
+    private int default_z_max = 20;
+
+    void Start()
+    {
+        //枠を作るメソッド呼び出し
+        CreateMapWaku();
+    }
+
+    //枠を作るメソッド
+    void CreateMapWaku()
+    {
+        //ループしながらz軸の上と下２列に枠を作ります
+        for (int dx = 0; dx <= default_x_max; dx++)
+        {
+            Instantiate(defaultWallPrefab, new Vector3(dx, 0, 0), Quaternion.identity);
+            Instantiate(defaultWallPrefab, new Vector3(dx, 0, default_z_max), Quaternion.identity);
+        }
+
+        //同じくx軸に右と左に枠を作ります
+        for (int dz = 0; dz <= default_z_max; dz++)
+        {
+            Instantiate(defaultWallPrefab, new Vector3(0, 0, dz), Quaternion.identity);
+            Instantiate(defaultWallPrefab, new Vector3(default_x_max, 0, dz), Quaternion.identity);
+        }
+
+    }
+}
